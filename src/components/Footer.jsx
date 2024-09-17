4e31609f196f9a6cf4860ec71275dd4ccb4bdb18
khaledabdalslam99 <khaledabdalslam99@gmail.com>
2024-09-17 16:14:20 -0400

    footer edited

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,32 +5,44 @@ import { FaFacebookF } from "react-icons/fa";
 import { FaPinterestP } from "react-icons/fa";
 export const Footer = () => {
   return (
-    <div className=' w-full pt-1 bg-gradient-to-r from-[#8BF2B5] to-[#35C8EB] md:h-[10px]'>
-      <div className='w-full bg-black md:h-[195px] flex  gap-[155px]'>
-        <div className='pl-[80px] pt-[30px] pb-[50px] '>
-          <p className=' text-white font-light text-[24px] mb-[6px]'>About</p>
-          <p className=' text-white font-light '>Home</p>
-          <p className=' text-white font-light '>Product</p>
-        </div>
-        <div className='pl-[80px] pt-[30px] pb-[50px] '>
-          <p className=' text-white font-light text-[24px] mb-[6px]'>Support</p>
-          <p className=' text-white font-light '>Activate</p>
-          <p className=' text-white font-light '>Login</p>
-        </div>
-        <div className='pl-[80px] pt-[30px] pb-[50px] '>
-          <p className=' text-white font-light text-[24px] mb-[6px] '>Find us on</p>
-          <div className='flex gap-[20px]'>
-            <FaYoutube className='text-white text-4xl' />
-            <FaTwitter className='text-white text-4xl' />
-            <FaFacebookF className='text-white text-4xl' />
-            <FaPinterestP className='text-white text-4xl' />
-          </div>
-        </div>
-          <hr className='mb-1 border-white'/>
-          <div>
-
-          </div>
+    <>
+      <div className=' w-full  bg-gradient-to-r from-[#8BF2B5] to-[#35C8EB] h-[10px] '>
       </div>
-    </div>
+      <div className='bg-black'>
+        <div className='flex w-full md:flex-row flex-col  justify-start items-start bg-black' >
+
+          <div className=' bg-black md:h-[195px] flex md:flex-row md:mr-40 md:gap-[155px]'>
+            <div className='md:pl-[80px] pl-[25px] pt-[30px] pb-[50px] '>
+              <p className=' text-white font-light text-[24px] mb-[6px]'>About</p>
+              <p className=' text-white font-light '>Home</p>
+              <p className=' text-white font-light '>Product</p>
+            </div>
+            <div className='md:pl-[80px] pl-[25px] pt-[30px] pb-[50px] '>
+              <p className=' text-white font-light text-[24px] mb-[6px]'>Support</p>
+              <p className=' text-white font-light '>Activate</p>
+              <p className=' text-white font-light '>Login</p>
+            </div>
+          </div>
+          <div className='md:pl-[80px] pl-[25px] pt-[30px] pb-[50px] '>
+            <p className=' text-white font-light text-[24px] '>Find us on</p>
+            <div className='flex gap-[20px]'>
+              <FaYoutube className='text-white text-4xl' />
+              <FaTwitter className='text-white text-4xl' />
+              <FaFacebookF className='text-white text-4xl' />
+              <FaPinterestP className='text-white text-4xl' />
+            </div>
+          </div>
+        </div>
+        <hr className='border-slate-500 ' />
+
+        <div className="bg-image bg-cover bg-center bg-black w-full h-[340px] overflow-x-hidden md:h-[220px] relative" >
+          <img src='footer-bg.png' className='contrast-150 absolute md:top-8 top-56 md:scale-[1] scale-[4]' />
+          <div className='w-full h-full'>
+            <img src="logo-footer.png" alt="logo" />
+          </div>
+        </div>
+      </div>
+    </>
+
   )
 }
